8337339017a463ea85fc623145c9ac9246dfec41
Praful-a <prafuldubey27@gmail.com>
2020-03-24 16:49:03 +0530

    fixes the error in userprofileserializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,20 +3,27 @@ from django.contrib.auth.models import User
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    """A Serializer for our user profile objects."""
+    """Creating user and updating"""
     class Meta:
         model = User
         fields = ['id', 'url', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
-        def create(self, validated_data):
-            """Create and return a new user."""
-            user = UserProfile(
-                username=validated_data['username'],
-                email=validated_data['email']
-            )
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
 
-            user.set_password(validated_data['password'])
-            user.save()
+        user.set_password(validated_data['password'])
+        user.save()
 
-            return user
+        return user
+
+    def update(self, instance, validated_data):
+        """Updating the profile."""
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+
+        return instance
